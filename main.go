d625172561d3705bc1f247404a86164d19e15d8b
QiaoShi <qiaoshi003@ke.com>
2020-04-10 17:48:12 +0800

    .

--- main.go
+++ main.go
@@ -10,5 +10,18 @@ func main() {
 	r.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>hello nice</h1>")
 	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
 	r.RUN(":8080")
 }
