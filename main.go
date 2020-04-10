6de92d482f4e256f1e512e09d2374b4b038e8a06
QiaoShi <qiaoshi003@ke.com>
2020-04-10 16:26:53 +0800

    定义路由表和路由映射处理方法

--- main.go
+++ main.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"xgg/xgg"
 )
 
-type Engine struct{}
-
-func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/":
-		fmt.Fprintf(w, "url.path=%q\n", req.URL.Path)
-	case "/hello":
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%s]=%q\n", k, v)
-		}
-	default:
-		fmt.Fprintf(w, "404 not found %s\n", req.URL)
-	}
-}
-
 func main() {
-	engine := new(Engine)
-	log.Fatal(http.ListenAndServe(":8080", engine))
+	r := xgg.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+	r.GET("/nice", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	r.RUN(":8080")
 }
